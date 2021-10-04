3034a919adf67b4fbf3b8084e816080972b26dc2
KIMTEAYANG123 <explore2012@naver.com>
2021-10-05 01:32:14 +0900

    회원가입 페이지 css

--- client/src/components/UserJoin.js
+++ client/src/components/UserJoin.js
@@ -3,10 +3,31 @@ import styled from 'styled-components'
 
 const JoinContents = styled.div`
     display: flex;
-    flex-direction: column;
     max-width:1000px;
+    height: 888px;
+    justify-content: center;
     color: white;
     margin: 0 auto;
+`
+const Main = styled.div`
+    display: flex;
+    margin: auto 0;
+    width:320px;
+    flex-direction: column;
+`
+const Input = styled.input`
+    border: none;
+    border-radius: 3px;
+    margin-bottom: 1rem;
+    margin-top: 1rem;
+    padding: 1rem;
+`
+const Button = styled.button`
+    background-color: #1E90FF;
+    border-radius: 10px;
+    color: white;
+    padding: 1rem;
+    border: none;
 `
 function UserJoin() {
     const [email, setEmail] = useState('')
@@ -29,15 +50,17 @@ function UserJoin() {
     return (
         <div>
             <JoinContents>
-                <label>이메일</label>
-                <input type='email' name='email' onChange={onChange} value={email}/>
+                <Main>
+                    <label>이메일</label>
+                    <Input type='email' name='email' onChange={onChange} value={email}/>
 
-                <label>비밀번호</label>
-                <input type='password' name='password'  onChange={onChange} value={password}/>
+                    <label>비밀번호</label>
+                    <Input type='password' name='password'  onChange={onChange} value={password}/>
 
-                <label>비밀번호 확인</label>
-                <input type='password' name='passwordCheck' onChange={onChange} value={passwordCheck}/>
-                <button>회원가입</button>
+                    <label>비밀번호 확인</label>
+                    <Input type='password' name='passwordCheck' onChange={onChange} value={passwordCheck}/>
+                    <Button>회원가입</Button>
+                </Main>
             </JoinContents>
         </div>
     )
